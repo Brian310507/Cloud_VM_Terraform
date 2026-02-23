7237ab6c480704dd51b30ac2dfad9c8beb52717d
Brian310507 <alexandre.roques@limayrac.fr>
2026-02-23 17:26:44 +0100

    Update outputs.tf to add resource group ID, load balancer public IP, and VMs private IPs outputs for improved visibility and management of Azure resources.

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
-output "vm_public_ips" {
-  description = "Public IP addresses of the VMs"
-  value       = azurerm_public_ip.pip[*].ip_address
+# Affiche l'ID du groupe de ressources
+output "resource_group_id" {
+  value = azurerm_resource_group.tp_rg.id
 }
 
-output "lb_public_ip" {
-  description = "Public IP address of the load balancer"
-  value       = azurerm_public_ip.lb_pip.ip_address
+# Affiche l'IP publique du Load Balancer (C'est elle que vous devez curl)
+output "load_balancer_public_ip" {
+  value = azurerm_public_ip.lb_pip.ip_address
+}
+
+# Affiche les IPs privées des VMs
+output "vms_private_ips" {
+  value = azurerm_network_interface.vm_nic[*].private_ip_address
 }
