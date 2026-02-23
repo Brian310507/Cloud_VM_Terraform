d6a81953278bd4f6e4df92d38e657ee0e1abde8c
Brian310507 <alexandre.roques@limayrac.fr>
2026-02-23 17:53:21 +0100

    Refactor provider configuration by moving Azure provider settings from versions.tf to providers.tf, ensuring a cleaner structure and improved organization of Terraform files.

--- providers.tf
+++ providers.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.28.0"
-    }
-  }
-}
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
--- versions.tf
+++ versions.tf
@@ -1,4 +1,8 @@
 terraform {
-  required_version = ">= 1.5.0"
-
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.28.0"
+    }
+  }
 }
