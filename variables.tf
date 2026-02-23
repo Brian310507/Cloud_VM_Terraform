42a13c96d51d090a117e6e6abcae720b06e2caa8
Brian310507 <alexandre.roques@limayrac.fr>
2026-02-23 16:13:39 +0100

    Add variables.tf to define Terraform variables for Azure deployment

---
+++ variables.tf
@@ -0,0 +1,19 @@
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "francecentral"
+}
+
+variable "prefix" {
+  description = "Prefix for resource names"
+  type        = string
+  default     = "tp-dev-bsi"
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for VMs"
+  type        = string
+}
+
+
