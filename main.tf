5049a39c3961df3d8caf956c0d9e98b65b508afc
Brian310507 <alexandre.roques@limayrac.fr>
2026-02-23 16:17:49 +0100

    Refactor main.tf to create resource group, virtual network, and subnet with updated naming conventions and tags. Removed unused resources for a cleaner configuration.

--- main.tf
+++ main.tf
@@ -1,162 +1,31 @@
-resource "azurerm_resource_group" "rg" {
+
+# 1. Création du Groupe de Ressources
+resource "azurerm_resource_group" "tp_rg" {
   name     = "${var.prefix}-rg"
   location = var.location
+
+  tags = {
+    environment = "tp"
+    managed_by  = "terraform"
+  }
 }
 
-resource "azurerm_virtual_network" "vnet" {
+# 2. Création du Réseau Virtuel (VNET)
+resource "azurerm_virtual_network" "tp_vnet" {
   name                = "${var.prefix}-vnet"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.tp_rg.location
+  resource_group_name = azurerm_resource_group.tp_rg.name
+
+  tags = {
+    environment = "tp"
+  }
 }
 
-resource "azurerm_subnet" "subnet" {
+# 3. Création du Sous-réseau (Subnet)
+resource "azurerm_subnet" "tp_subnet" {
   name                 = "${var.prefix}-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.tp_rg.name
+  virtual_network_name = azurerm_virtual_network.tp_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = "${var.prefix}-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "allow_ssh"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "0.0.0.0/0"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "allow_http"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "0.0.0.0/0"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_public_ip" "pip" {
-  count               = 2
-  name                = "${var.prefix}-pip-${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Basic"
-}
-
-resource "azurerm_network_interface" "nic" {
-  count               = 2
-  name                = "${var.prefix}-nic-${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip[count.index].id
-  }
-}
-
-resource "azurerm_public_ip" "lb_pip" {
-  name                = "${var.prefix}-lb-pip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_lb" "lb" {
-  name                = "${var.prefix}-lb"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Standard"
-
-  frontend_ip_configuration {
-    name                 = "public-ip-config"
-    public_ip_address_id = azurerm_public_ip.lb_pip.id
-  }
-}
-
-resource "azurerm_lb_backend_address_pool" "lb_backend_pool" {
-  name            = "${var.prefix}-lb-backendpool"
-  loadbalancer_id = azurerm_lb.lb.id
-}
-
-resource "azurerm_lb_probe" "lb_probe" {
-  name                = "${var.prefix}-lb-probe"
-  loadbalancer_id     = azurerm_lb.lb.id
-  protocol            = "Tcp"
-  port                = 80
-  interval_in_seconds = 5
-  number_of_probes    = 2
-}
-
-resource "azurerm_lb_rule" "lb_rule" {
-  name                           = "${var.prefix}-lb-rule-http"
-  loadbalancer_id                = azurerm_lb.lb.id
-  protocol                       = "Tcp"
-  frontend_port                  = 80
-  backend_port                   = 80
-  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lb_backend_pool.id]
-  probe_id                       = azurerm_lb_probe.lb_probe.id
-  frontend_ip_configuration_name = "public-ip-config"
-}
-
-resource "azurerm_network_interface_security_group_association" "nic_nsg" {
-  count                     = 2
-  network_interface_id      = azurerm_network_interface.nic[count.index].id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-resource "azurerm_network_interface_backend_address_pool_association" "nic_lb_backend" {
-  count                   = 2
-  network_interface_id    = azurerm_network_interface.nic[count.index].id
-  ip_configuration_name   = "internal"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.lb_backend_pool.id
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  count               = 2
-  name                = "${var.prefix}-vm-${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  size                = "Standard_B1s"
-
-  admin_username = "azureuser"
-
-  network_interface_ids = [
-    azurerm_network_interface.nic[count.index].id
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
-    version   = "latest"
-  }
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-
-  disable_password_authentication = true
 }
